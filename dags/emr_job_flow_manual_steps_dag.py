d7e39ece37575ffc70877f3fffc7cbc3b1dd8581
ASammyA <86346618+ASammyA@users.noreply.github.com>
2021-11-16 17:50:14 -0700

    Update emr_job_flow_manual_steps_dag.py

--- dags/emr_job_flow_manual_steps_dag.py
+++ dags/emr_job_flow_manual_steps_dag.py
@@ -40,7 +40,7 @@ DEFAULT_ARGS = {
     'email_on_retry': False
 }
 
-CLUSTER_ID = 'j-1GTSVNDFNSNPZ'
+CLUSTER_ID = 'j-1WAZZDKEAFUW7'
 
 def retrieve_s3_file(**kwargs):
     s3_location = kwargs['dag_run'].conf['s3_location'] 
