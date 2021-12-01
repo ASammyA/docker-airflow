058ef4c0b2a1eb19b3ba640cef1b5585935e5ce6
EC2 Default User <ec2-user@ip-172-31-82-253.ec2.internal>
2021-12-01 11:03:13 +0000

    pyspark dag: added functions to dynamically watch EMR clusters and grab available cluster Id

--- dags/emr_job_flow_manual_steps_dag_pyspark.py
+++ dags/emr_job_flow_manual_steps_dag_pyspark.py
@@ -30,6 +30,7 @@ from airflow.contrib.operators.emr_terminate_job_flow_operator \
         import EmrTerminateJobFlowOperator
 from airflow.operators.python_operator import PythonOperator
 
+import boto3
 
 DEFAULT_ARGS = {
     'owner': 'airflow',
@@ -40,7 +41,54 @@ DEFAULT_ARGS = {
     'email_on_retry': False
 }
 
-CLUSTER_ID = 'j-2WNGO5199Z6TQ'
+def get_avilable_cluster_id():
+    clusters = boto3.Session().client('emr', region_name= 'us-east-1').list_clusters()['Clusters']
+    for x in clusters:
+        if (x['Status']['State']) in ['RUNNING','WAITING']:
+            return x['Id']
+        else:
+            running_cluster = False
+
+def watch_clusters(): 
+    running = True  #Initial/controlling condition for the while loop
+    seconds_lapsed = 0  #Counter to track how long the run takes
+    timeout = 1200  #Maximum time in seconds we are allowing the run to take
+
+    while running:
+
+        if get_avilable_cluster_id() != None:
+
+            print('Found an available cluster. Returning Cluster Id.')
+            return get_avilable_cluster_id()
+
+        elif seconds_lapsed == timeout:
+
+            #output timeout report
+            print("timeout")
+
+            #break the loop
+            running = False
+        
+        elif get_avilable_cluster_id() == None:
+
+            #wait 5 seconds and update the counter, continue the loop
+            time.sleep(5)
+            seconds_lapsed += 5
+            m = int(seconds_lapsed / 60)
+            s = seconds_lapsed % 60
+
+            print(f'No available cluster running. Waiting 5 seconds and checking again. Total time lapsed: {m}:{s}')
+            continue
+
+        else:
+
+            #output error
+            print("unknown error")
+
+            #break the loop
+            running = False
+
+CLUSTER_ID = watch_clusters()
 
 def retrieve_s3_file(**kwargs):
     s3_location = kwargs['dag_run'].conf['s3_location'] 
